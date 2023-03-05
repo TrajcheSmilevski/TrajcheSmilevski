c882d9893530f6042837189911f0b11a41339c6a
tralesmile <trajce_2000@hotmail.com>
2023-03-05 20:10:09 +0100

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+//    }
+}
